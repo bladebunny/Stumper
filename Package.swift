816983150e5f0968cd97d24b1de7352c9c5556ed
Tim Brooks <tim.a.brooks@disney.com>
2024-05-20 10:48:10 -0700

    version updates

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Stumper",
     platforms: [
-        .macOS(.v10_15), .iOS(.v16), .tvOS(.v14)
+        .macOS(.v13), .iOS(.v16), .tvOS(.v16)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
